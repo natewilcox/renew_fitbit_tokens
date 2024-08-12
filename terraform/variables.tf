44e93d360423bd8185c5c065fcd79db02f8a8964
natewilcox@gmail.com <natewilcox@gmail.com>
2024-08-11 23:51:58 -0400

    update actions

---
+++ terraform/variables.tf
@@ -0,0 +1,12 @@
+
+variable "fitbit_client_id" {
+    description = "Fitbit client id"
+    type        = string
+    sensitive   = true
+}
+
+variable "fitbit_client_secret" {
+    description = "Fitbit client secret"
+    type        = string
+    sensitive = true
+}
